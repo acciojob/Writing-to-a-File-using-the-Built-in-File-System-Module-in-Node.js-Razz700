a99eef7451201d205c644ef7a0cfd10e24b7d15f
Razzakh <mrazzakh123@gmail.com>
2024-10-16 19:29:17 +0530

    first

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const http=require('http');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-const data=fs.readFileSync('./output.txt','utf-8');
-console.log(data);
+fs.writeFile('./output.txt',"Hello, World!",(err)=>{
+    if(err){
+       return console.log(err);
+    }else{
+        console.log('File written successfully!');
+    }
+    
+})
 
