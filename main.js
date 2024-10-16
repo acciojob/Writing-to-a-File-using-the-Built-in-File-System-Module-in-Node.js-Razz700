bdb3da2d2e0f10103974486148a2066916cd5779
Razzakh <mrazzakh123@gmail.com>
2024-10-16 19:21:43 +0530

    now

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ const fs = require('fs');
 const http=require('http');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
+const data=fs.readFileSync('./output.txt','utf-8');
+console.log(data);
 
